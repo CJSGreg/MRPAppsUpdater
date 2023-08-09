61f7b8f982120bdcb6023b892d227ef58d936c6e
Greg <greg@cjs-it.com>
2023-08-09 17:13:18 +0100

    Re-commit of apps folder

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from github import Github
 
+g = Github()
 
+repo = g.get_repo("CJSGreg/MRPAppsUpdater")
 
+contents = repo.get_contents("maintained_apps/MRPDashboards")
+
+for content in contents:
+    decoded = content.decoded_content
+
+    print(decoded)
